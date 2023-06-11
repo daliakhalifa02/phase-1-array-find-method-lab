45a1a84e91de6e29920da51de4a5dac303281511
Dalia Khalifa <daliakhalifa02@gmail.com>
2023-06-11 22:47:28 +0300

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function superbowlWin(records) {
+    const winningRecord = records.find(record => record.result === "W");
+    return winningRecord ? winningRecord.year : undefined;
+  }
+
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+]
+  
+console.log(superbowlWin(record));
